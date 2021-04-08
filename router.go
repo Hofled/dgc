e45f6f1cefb54a6298a3245a3d831f4ae76c7573
Hofled <ofri.herold@gmail.com>
2021-04-08 17:07:46 +0300

    Added ability to initialize guild specific command router

--- router.go
+++ router.go
@@ -60,9 +60,46 @@ func (router *Router) InitializeStorage(name string) {
 	router.Storage[name] = newObjectsMap()
 }
 
+// InitializeGuildHandler initializes the message event listener for a specific guild
+func (router *Router) InitializeGuildHandler(session *discordgo.Session, guildID string) {
+	session.AddHandler(router.GuildHandler(guildID))
+}
+
 // Initialize initializes the message event listener
 func (router *Router) Initialize(session *discordgo.Session) {
 	session.AddHandler(router.Handler())
+}
+
+// GuildHandler provides the discordgo handler for the given router in the specified guild
+func (router *Router) GuildHandler(guildID string) func(*discordgo.Session, *discordgo.MessageCreate) {
+	return func(session *discordgo.Session, event *discordgo.MessageCreate) {
+		// Define useful variables
+		message := event.Message
+		content := message.Content
+
+		// Check if the message was sent by a bot
+		if message.Author.Bot && !router.BotsAllowed {
+			return
+		}
+
+		// Execute the ping handler if the message equals the current bot's mention
+		if (content == "<@!"+session.State.User.ID+">" || content == "<@"+session.State.User.ID+">") && router.PingHandler != nil {
+			router.PingHandler(&Ctx{
+				Session:   session,
+				Event:     event,
+				Arguments: ParseArguments(""),
+				Router:    router,
+			})
+			return
+		}
+
+		// Check for guild ID, handle messages only sent in the specified guild
+		if message.GuildID != guildID {
+			return
+		}
+
+		handleEventContent(router, content, session, event)
+	}
 }
 
 // Handler provides the discordgo handler for the given router
@@ -88,58 +125,62 @@ func (router *Router) Handler() func(*discordgo.Session, *discordgo.MessageCreat
 			return
 		}
 
-		// Get rid of additional spaces
-		content = strings.Trim(content, " ")
+		handleEventContent(router, content, session, event)
+	}
+}
 
-		// Check if the message is empty after the prefix processing
-		if content == "" {
-			return
+func handleEventContent(router *Router, content string, session *discordgo.Session, event *discordgo.MessageCreate) {
+	// Get rid of additional spaces
+	content = strings.Trim(content, " ")
+
+	// Check if the message is empty after the prefix processing
+	if content == "" {
+		return
+	}
+
+	// Split the messages at any whitespace
+	parts := regexSplitting.Split(content, -1)
+
+	var commandText string
+	var hasPrefix bool
+	var prefixes []string
+	// Check if the message starts with a command name
+	for _, command := range router.Commands {
+		commandText = parts[0]
+		prefixes = router.Prefixes
+
+		// Use command specific prefixes if any exist
+		if len(command.Prefixes) > 0 {
+			prefixes = command.Prefixes
 		}
 
-		// Split the messages at any whitespace
-		parts := regexSplitting.Split(content, -1)
-
-		var commandText string
-		var hasPrefix bool
-		var prefixes []string
-		// Check if the message starts with a command name
-		for _, command := range router.Commands {
-			commandText = parts[0]
-			prefixes = router.Prefixes
-
-			// Use command specific prefixes if any exist
-			if len(command.Prefixes) > 0 {
-				prefixes = command.Prefixes
-			}
-
-			// Check prefixes if any prefixes were specified
-			if len(prefixes) > 0 {
-				hasPrefix, commandText = stringHasPrefix(commandText, prefixes, command.IgnoreCase)
-				if !hasPrefix {
-					continue
-				}
-			}
-
-			// Check if the first part is the current command
-			if !stringArrayContains(getIdentifiers(command), commandText, command.IgnoreCase) {
+		// Check prefixes if any prefixes were specified
+		if len(prefixes) > 0 {
+			hasPrefix, commandText = stringHasPrefix(commandText, prefixes, command.IgnoreCase)
+			if !hasPrefix {
 				continue
 			}
-
-			content = strings.Join(parts[1:], " ")
-
-			// Define the command context
-			ctx := &Ctx{
-				Session:       session,
-				Event:         event,
-				Arguments:     ParseArguments(content),
-				CustomObjects: newObjectsMap(),
-				Router:        router,
-				Command:       command,
-			}
-
-			// Trigger the command
-			command.trigger(ctx)
 		}
+
+		// Check if the first part is the current command
+		if !stringArrayContains(getIdentifiers(command), commandText, command.IgnoreCase) {
+			continue
+		}
+
+		content = strings.Join(parts[1:], " ")
+
+		// Define the command context
+		ctx := &Ctx{
+			Session:       session,
+			Event:         event,
+			Arguments:     ParseArguments(content),
+			CustomObjects: newObjectsMap(),
+			Router:        router,
+			Command:       command,
+		}
+
+		// Trigger the command
+		command.trigger(ctx)
 	}
 }
 
